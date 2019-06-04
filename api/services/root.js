de015e232d697e0639a570c045980cb8aedc5cd5
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2019-06-04 23:51:43 +0200

    Update root.js

--- api/services/root.js
+++ api/services/root.js
@@ -2,7 +2,7 @@
 const sharkd_dict = require('../custom_module/sharkd_dict');
 
 module.exports = function (fastify, opts, next) {
-  fastify.get('/json', function (request, reply) {
+  fastify.get('/webshark/json', function (request, reply) {
 
     sharkd_dict.send_req(request.query).then((data) => {
       reply.send(data);
