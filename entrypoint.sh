8be89f822e629174a1c9bf4527c25c022925e47d
Eldon Koyle <eldon.koyle@corelight.com>
2024-05-04 01:38:32 -0600

    Set ownership on /captures (#37)
    Set inital ownership on /captures in dockerfile. Run entrypoint script
    as root so that it can change `root:root` ownership to `node:node` in
    the case of docker compose, then use `su` to drop back to the node user.
    
    Fixes #36

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,12 @@
 # sharkd daemon fails to start if the socket already exists
 rm "$SHARKD_SOCKET"
 
-exec npm start
+dir_owner=$(stat -c "%U:%G" "${CAPTURES_PATH}")
+
+if [ "x${dir_owner}" = "xroot:root" ]; then
+    # assume CAPTURES_PATH owned by root:root is unintentional
+    # (probably created by docker-compose)
+    chown node: "${CAPTURES_PATH}"
+fi
+
+exec su node -c "npm start"
